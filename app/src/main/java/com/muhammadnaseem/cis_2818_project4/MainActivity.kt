d208f6e993a850d75680481fa03221a9147fbce0
Jozsua <jkmekis@gmail.com>
2023-11-22 22:16:06 -0500

    Josh's first commit

--- app/src/main/java/com/muhammadnaseem/cis_2818_project4/MainActivity.kt
+++ app/src/main/java/com/muhammadnaseem/cis_2818_project4/MainActivity.kt
@@ -2,12 +2,63 @@ package com.muhammadnaseem.cis_2818_project4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawarLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
 
-//test comment//
+        drawarLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawarLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawarLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            R.id.nav_settings -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment()).commit()
+            R.id.nav_search -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SearchFragment()).commit()
+            R.id.nav_favorites -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FavoritesFragment()).commit()
+        }
+        drawarLayout.closeDrawer(GravityCompat.START)
+        return  true
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawarLayout.isDrawerOpen(GravityCompat.START)) {
+            drawarLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+}
