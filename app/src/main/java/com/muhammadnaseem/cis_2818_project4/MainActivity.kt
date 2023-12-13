b86b09394e90ff1ec84c89485dc98fff3fef6b81
muhammadn9 <20mnaseem@gmail.com>
2023-12-12 21:14:03 -0500

    muhammad's proj 3 updated

--- app/src/main/java/com/muhammadnaseem/cis_2818_project4/MainActivity.kt
+++ app/src/main/java/com/muhammadnaseem/cis_2818_project4/MainActivity.kt
@@ -1,84 +1,129 @@
 package com.muhammadnaseem.cis_2818_project4
 
-import com.android.volley.Request
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.muhammadnaseem.cis_2818_project4.databinding.ActivityMainBinding
+import org.json.JSONException
 import org.json.JSONObject
-import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var cryptoSpinner: Spinner
+    private lateinit var cryptoInfoLayout: LinearLayout
+    private lateinit var cryptoNameTextView: TextView
+    private lateinit var cryptoSymbolTextView: TextView
+    private lateinit var cryptoSupplyTextView: TextView
+    private lateinit var cryptoPriceTextView: TextView
+    private lateinit var cryptoPercentChangeTextView: TextView
+    private lateinit var loadingProgressBar: ProgressBar // Declare loadingProgressBar here
+
+    private lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        cryptoSpinner = findViewById(R.id.cryptoSpinner)
+        cryptoInfoLayout = findViewById(R.id.cryptoInfoLayout)
+        cryptoNameTextView = findViewById(R.id.cryptoNameTextView)
+        cryptoSymbolTextView = findViewById(R.id.cryptoSymbolTextView)
+        cryptoSupplyTextView = findViewById(R.id.cryptoSupplyTextView)
+        cryptoPriceTextView = findViewById(R.id.cryptoPriceTextView)
+        cryptoPercentChangeTextView = findViewById(R.id.cryptoPercentChangeTextView)
+        loadingProgressBar = findViewById(R.id.loadingProgressBar) // Initialize loadingProgressBar
 
+        requestQueue = Volley.newRequestQueue(this)
 
-        val queue = Volley.newRequestQueue(this)
+        // Populate the Spinner with cryptocurrency names
+        val cryptoNames = listOf("Bitcoin", "Ethereum", "Litecoin")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cryptoNames)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        cryptoSpinner.adapter = adapter
+
+        // Set a listener for item selection in the Spinner
+        cryptoSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedCryptoName = cryptoNames[position]
+                fetchCryptoData(selectedCryptoName)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+    }
+
+    private fun fetchCryptoData(cryptoName: String) {
+        cryptoInfoLayout.visibility = View.GONE
+        loadingProgressBar.visibility = View.VISIBLE
+
         val url = "https://api.coincap.io/v2/assets"
-        val request = StringRequest(
-            Request.Method.GET, url, {
-                    response ->
-                val dataObject = JSONObject(response)
-                val dataArray = dataObject.getJSONArray("data")
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.d("CryptoApp", "API Response: $response") // Log entire response for inspection
+                try {
+                    // Parse the JSON response and log the details
+                    val data = response.getJSONArray("data")
+                    var found = false
+                    for (i in 0 until data.length()) {
+                        val cryptoObject = data.getJSONObject(i)
+                        val nameFromAPI = cryptoObject.getString("name")
 
-                val coins = ArrayList<CoinType>()
+                        // Log the names for debugging
+                        Log.d("CryptoApp", "Name from API: $nameFromAPI, Selected Name: $cryptoName")
 
-                for (i in 0 until dataArray.length()) {
-                    val coin = dataArray.getJSONObject(i)
+                        if (nameFromAPI == cryptoName) {
+                            Log.d("CryptoApp", "Crypto Data: $cryptoObject") // Log cryptocurrency's data
+                            updateCryptoInfo(cryptoObject)
+                            found = true
+                            break
+                        }
+                    }
 
-                    val coinName = coin.getString("name")
-                    val coinSymbol = coin.getString("symbol")
-                    val coinSupply = coin.getString("supply").toFloat().toInt()
-                    val coinPrice = coin.getString("priceUsd").toFloat()
-                    val coinChange = coin.getString("changePercent24Hr").toFloat()
-
-                    val coinObject = CoinType(coinName, coinSymbol, coinSupply, coinPrice, coinChange)
-
-                    coins.add(coinObject)
-
-                    Log.i("Volley Data", "${coinObject.name} ${coinObject.symbol} ${coinObject.supply}")
+                    if (!found) {
+                        Log.d("CryptoApp", "No match found for $cryptoName")
+                        // Handle the case where no match is found
+                        // You might want to clear or hide the displayed information in this case
+                    }
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                    Log.e("CryptoApp", "JSON parsing error: ${e.message}")
+                } finally {
+                    loadingProgressBar.visibility = View.GONE
                 }
-
-//                val coinAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, coins)
-//                binding.coinSpinner.adapter = coinAdapter
-
-//                binding.coinSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-//                    override fun onItemSelected(
-//                        parent: AdapterView<*>?,
-//                        view: View?,
-//                        position: Int,
-//                        id: Long
-//                    ) {
-//                        binding.tvName.text = coins.get(position).name
-//                        binding.tvSymbol.text = coins.get(position).symbol
-//                        binding.tvSupply.text = coins.get(position).supply.toString()
-//                        binding.tvPrice.text = coins.get(position).price.toString()
-//                        binding.tvChange.text = coins.get(position).change.toString()
-//                    }
-//
-//                    override fun onNothingSelected(parent: AdapterView<*>?) {
-//                        TODO("Not yet implemented")
-//                    }
-//
-//                }
-
             },
-            Response.ErrorListener { Log.i("Volley Error", "Error Ocurred") }
-        )
+            Response.ErrorListener { error ->
+                error.printStackTrace()
+                Log.e("CryptoApp", "Volley error: ${error.message}")
+                loadingProgressBar.visibility = View.GONE
+            })
 
-        queue.add(request)
+        // Add the request to the RequestQueue
+        requestQueue.add(jsonObjectRequest)
+    }
 
+    private fun updateCryptoInfo(cryptoObject: JSONObject) {
+        // Update TextViews with cryptocurrency information
+        cryptoNameTextView.text = cryptoObject.getString("name")
+        cryptoSymbolTextView.text = cryptoObject.getString("symbol")
+        cryptoSupplyTextView.text = "Supply: ${cryptoObject.getDouble("supply").toInt()}"
+        cryptoPriceTextView.text = "Price: $${String.format("%.2f", cryptoObject.getDouble("priceUsd"))}"
+        cryptoPercentChangeTextView.text =
+            "Change (24h): ${String.format("%.2f", cryptoObject.getDouble("changePercent24Hr"))}%"
+
+        // Make the cryptoInfoLayout visible
+        cryptoInfoLayout.visibility = View.VISIBLE
     }
 }
